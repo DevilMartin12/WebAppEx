661093a5b16c79196bc5d5a32301af6edab71dd2
Martin0134 <m.panevdms5@gmail.com>
2023-11-02 13:26:17 +0200

    Revert "Update3fix"
    This reverts commit 01cf2cd72cff18fd9d114a0e2dc4229277af6b2b.

--- index.php
+++ index.php
@@ -5,5 +5,11 @@
     </head>
     <body>
         Здравей :)
+        <?php
+            $target = "contacts.php";
+            $linkname = "mylink";
+
+            link($target, $linkname);
+        ?>
     </body>
 </html>
