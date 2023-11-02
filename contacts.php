135a346414440f1049da84215dbed60204131d54
Martin0134 <m.panevdms5@gmail.com>
2023-11-02 13:15:35 +0200

    Update2

---
+++ contacts.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        Здравей :)
+    </body>
+</html>
+    <?php
+        $target = "text.txt";
+        $linkname = "mylink";
+
+        link($target, $linkname);
+        ?>
